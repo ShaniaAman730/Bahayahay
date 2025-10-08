e057d2d4c8956379ac597c1cd5f59758180530a3
ShaniaAman730 <shaniaaman98@gmail.com>
2025-10-09 06:03:59 +0800

    fixed notifications_channel.rb

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -2,6 +2,6 @@ class NotificationsChannel < ApplicationCable::Channel
   #def subscribed
    # stream_from "notifications_#{current_user.id}"
     #Rails.logger.info "Subscribed to notifications_#{current_user.id}"
-  end
+  #end
 end
 
