7e9652f327d97c1c326a3800d7ea516a168aa65e
ShaniaAman730 <shaniaaman98@gmail.com>
2025-10-09 01:08:43 +0800

    tryna deploy 6

--- app/models/notification.rb
+++
@@ -1,7 +0,0 @@
-class Notification < ApplicationRecord
-  belongs_to :user
-  belongs_to :notifiable, polymorphic: true, optional: true
-
-  scope :unread, -> { where(read: false) }
-  scope :recent_first, -> { order(created_at: :desc) }
-end
