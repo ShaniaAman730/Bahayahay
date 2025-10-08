f641e1b4f79921e7e2adb15700f25d0dc5291ad9
ShaniaAman730 <shaniaaman98@gmail.com>
2025-10-09 03:10:05 +0800

    fixed puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,18 @@
 # Puma configuration for Render
 
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
 threads threads_count, threads_count
 
-environment ENV.fetch("RAILS_ENV") { "production" }
+environment ENV.fetch("RAILS_ENV", "production")
 
-# Only set the port; Render will route traffic correctly
-port ENV.fetch("PORT") { 10000 }
+# Render automatically sets PORT, default fallback 10000
+port ENV.fetch("PORT", 10000)
 
-# Workers for concurrency (optional)
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# Workers for concurrency
+workers ENV.fetch("WEB_CONCURRENCY", 2).to_i
 preload_app!
 
-# Allow puma to be restarted by `bin/rails restart`
+# Allow Puma to be restarted by `bin/rails restart`
 plugin :tmp_restart
 
 # Solid Queue plugin (if using)
@@ -20,3 +20,6 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
 # PID file (optional)
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+# On Render, bind to all interfaces (0.0.0.0)
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 10000)}"
